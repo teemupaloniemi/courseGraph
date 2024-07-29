da13244cafd55d20fb8f1ae80daf9890dd40c4d5
test <you@example.com>
2024-07-29 22:12:00 +0300

    added hierarchy

--- graph.js
+++ graph.js
@@ -46,8 +46,29 @@ function loadCourses() {
 let network;
 let container = document.getElementById('network');
 let options = {
+    nodes: { 
+        size: 500,
+    },
     edges: {
-        arrows: 'to'
+        arrows: 'to',
+        width: 16,
+        hoverWidth: 64,
+        selectionWidth: 64,
+        smooth: {
+            enabled: true,
+            type: 'continuous',
+        },
+    },
+    layout: { 
+        hierarchical: { 
+            enabled: true,
+            sortMethod: "directed",
+            levelSeparation: 1000,
+            nodeSpacing: 300,
+        }, 
+    }, 
+    physics: { 
+        enabled: false,
     },
 };
 
@@ -99,23 +120,25 @@ function renderNetwork(departmentIndex, index) {
         if (index === 29 || course.department === departmentIndex) {
           
             const nodeSize = getNodeSize(course.prerequisites.length);
-            const formattedLabel = formatLabel(course.name, 8);
-
+            const formattedLabel = formatLabel(course.name, 8); 
             if (!departmentNodes.some(node => node.id === course.id)) {
                 const backgroundColor = departmentColors[org_names.indexOf(course.department)]; 
                 let opacity = highlightedCourses.length === 0 || highlightedCourses.includes(course.id) ? 1.0 : 0.0; // Default opacity for all nodes if no text, otherwise reduce opacity for non-highlighted nodes
                 opacity *= levels.indexOf(course.level)*0.25
                 const rgbaColor = `rgba(${parseInt(backgroundColor.slice(-6, -4), 16)}, ${parseInt(backgroundColor.slice(-4, -2), 16)}, ${parseInt(backgroundColor.slice(-2), 16)}, ${opacity})`;
+                let prerequisitenes = coursePrerequisitenes(course);
+                if (prerequisitenes != 0) prerequisites = Math.round(Math.log(prerequisitenes));
                 departmentNodes.push({
                     id: course.id,
                     label: formattedLabel,
-                    size: nodeSize,
+                    level: 5*levels.indexOf(course.level) - prerequisitenes,
                     shape: 'circle',
                     font: {
                         multi: 'html',
-                        size: 14,
+                        size: 128,
                         face: 'arial',
-                        color: 'black'
+                        color: 'black',
+                        align: 'center',
                     },
                     shapeProperties: {
                         interpolation: false
@@ -162,7 +185,7 @@ loadDetails();
 
 const org_names = ['Kieli- ja viestintätieteiden laitos', 'Bio- ja ympäristötieteiden laitos', 'Liikuntatieteellinen tiedekunta', 'Yliopistopalvelut', 'Musiikin, taiteen ja kulttuurin tutkimuksen laitos', 'Humanistis-yhteiskuntatieteellinen tiedekunta', 'Psykologian laitos', 'Avoin yliopisto', 'Matematiikan ja tilastotieteen laitos', 'Informaatioteknologian tiedekunta', 'Kokkolan yliopistokeskus Chydenius - Kasvatustieteet', 'Kokkolan yliopistokeskus Chydenius - Yhteiskuntatieteet', 'Jyväskylän yliopiston kauppakorkeakoulu', 'Kokkolan yliopistokeskus Chydenius', 'Matemaattis-luonnontieteellinen tiedekunta', 'Yliopiston yhteiset', 'Avoimen tiedon keskus', 'Koulutuspalvelut', 'Historian ja etnologian laitos', 'Kemian laitos', 'Monikielisen akateemisen viestinnän keskus', 'Kokkolan yliopistokeskus Chydenius - Informaatioteknologia', 'Henkilöstöpalvelut', 'Kasvatustieteiden laitos', 'Yhteiskuntatieteiden ja filosofian laitos', 'Opettajankoulutuslaitos', 'Fysiikan laitos', 'Kasvatustieteiden ja psykologian tiedekunta', 'Jyväskylän yliopisto', 'Kaikki (Hidas!)']; 
 
-const levels = ['Muut opinnot', 'Aineopinnot', 'Perusopinnot', 'Syventävät opinnot', 'Jatko-opinnot', 'Ammattiopinnot']
+const levels = ['Muut opinnot', 'Perusopinnot', 'Aineopinnot', 'Syventävät opinnot', 'Jatko-opinnot', 'Ammattiopinnot']
 
 const controls = document.getElementById('controls');
 departmentColors.forEach((color, index) => {
