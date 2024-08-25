ff4218f873931f766c9639a31365210ceaa679f5
test <you@example.com>
2024-08-25 21:51:33 +0300

    load modules

--- download.sh
+++ download.sh
@@ -1,2 +1,3 @@
 wget http://users.jyu.fi/~tealjapa/ops/courses.json
 wget http://users.jyu.fi/~tealjapa/ops/reqs.json
+wget http://users.jyu.fi/~tealjapa/ops/modules.json
--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 pip3 install -r requirements.txt
 wget http://users.jyu.fi/~tealjapa/ops/courses.json
 wget http://users.jyu.fi/~tealjapa/ops/reqs.json
+wget http://users.jyu.fi/~tealjapa/ops/modules.json
 ./server.py
