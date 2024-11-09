cd384e989e54602de5f88cc507def6a42812a33a
test <you@example.com>
2024-11-09 16:24:16 +0200

    feat: update start.sh to load acm_classes

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@ pip3 install -r requirements.txt
 [ ! -f courses.json ] && wget http://users.jyu.fi/~tealjapa/ops/courses.json
 [ ! -f reqs.json ] && wget http://users.jyu.fi/~tealjapa/ops/reqs.json
 [ ! -f modules.json ] && wget http://users.jyu.fi/~tealjapa/ops/modules.json
-[ ! -f couse_acm.json ] && wget http://users.jyu.fi/~tealjapa/ops/course_acm.json
+[ ! -f course_acm.json ] && wget http://users.jyu.fi/~tealjapa/ops/course_acm.json
 ./server.py $1
